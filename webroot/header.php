a0eb6f684d30eb4784540a58ee7dd4021680b99a
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2017-01-07 23:58:46 +0100

    Remove unused scrollTo and spectrum.

--- webroot/header.php
+++ webroot/header.php
@@ -3,14 +3,11 @@
 	<meta name="keywords" content="<?php print Settings::get('metaTags'); ?>" />
 	<link rel="shortcut icon" type="image/x-icon" href="<?php print $layout_favicon;?>" />
 	<link rel="stylesheet" type="text/css" href="<?php print resourceLink("css/common.css");?>" />
-	<link rel="stylesheet" type="text/css" href="<?php print resourceLink("js/spectrum.css");?>" />
 	<link rel="stylesheet" href="<?php print resourceLink("css/font-awesome.min.css");?>">
 	
 	<script type="text/javascript" src="<?php print resourceLink("js/jquery.js");?>"></script>
 	<script type="text/javascript" src="<?php print resourceLink("js/tricks.js");?>"></script>
 	<script type="text/javascript" src="<?php print resourceLink("js/jquery.tablednd_0_5.js");?>"></script>
-	<script type="text/javascript" src="<?php print resourceLink("js/jquery.scrollTo-1.4.2-min.js");?>"></script>
-	<script type="text/javascript" src="<?php print resourceLink("js/spectrum.js");?>"></script>
 	<script type="text/javascript">
 		boardroot = <?php print json_encode($boardroot); ?>;
 	</script>
