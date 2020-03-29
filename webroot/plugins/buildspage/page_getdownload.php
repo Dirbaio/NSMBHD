c57d9528473573a92185d177d8b783f3fe292362
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2020-03-29 23:53:07 +0200

    Try to fix bad content-type in download

--- webroot/plugins/buildspage/page_getdownload.php
+++ webroot/plugins/buildspage/page_getdownload.php
@@ -16,33 +16,13 @@
 			die("No such file.");
 
 		$fsize = filesize($path);
-		$parts = pathinfo($path);
-		$ext = strtolower($parts["extension"]);
-		$download = true;
-
-		switch ($ext)
-		{
-			case "gif": $ctype="image/gif"; $download = false; break;
-			case "apng":
-			case "png": $ctype="image/png"; $download = false; break;
-			case "jpeg":
-			case "jpg": $ctype="image/jpg"; $download = false; break;
-			case "css": $ctype="text/css"; $download = false; break;
-			case "txt": $ctype="text/plain"; $download = false; break;
-			case "swf": $ctype="application/x-shockwave-flash"; $download = false; break;
-			case "pdf": $ctype="application/pdf"; $download = false; break;
-			default: $ctype="application/force-download"; break;
-		}
 
 		header("Pragma: public");
 		header("Expires: 0");
 		header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 		header("Cache-Control: private", false);
-		header("Content-Type: ".$ctype);
-		if($download)
-			header("Content-Disposition: attachment; filename=\"$fname\";");
-		else
-			header("Content-Disposition: filename=\"$fname\"");
+		header("Content-Type: application/zip");
+		header("Content-Disposition: attachment; filename=\"$fname\";");
 		header("Content-Transfer-Encoding: binary");
 		header("Content-Length: ".$fsize);
 
