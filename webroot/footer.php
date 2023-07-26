acabe43f24b52409532c13108c19ebbf8baba2d8
Jamie <134328492+compa-c@users.noreply.github.com>
2023-07-26 12:41:41 +0100

    Update footer.php
    wee, more acmlmboard-ey

--- webroot/footer.php
+++ webroot/footer.php
@@ -5,8 +5,8 @@ else echo '<a href="#" onclick="enableMobileLayout(-1); return false;" rel="nofo
 <br>
 <br>
 <?php $bucket = "footer"; include("./lib/pluginloader.php");?>
-Powered by <a href="http://abxd.dirbaio.net/">AcmlmBoard XD</a><br />
-By Dirbaio, xfix, Kawa, StapleButter, Nina, et al<br />
+<a href="http://github.com/Dirbaio/NSMBHD">ABXD 3.0+nsmbhd</a> (2023-07-26)<br />
+by Dirbaio, Nadia, Arisotura, xfix, Kawa et al.<br />
 AcmlmBoard &copy; Jean-Fran&ccedil;ois Lapointe<br />
 <?php print __("<!-- English translation by The ABXD Team -->")?>
 
