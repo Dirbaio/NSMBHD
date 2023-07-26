71ce12d0343660237d5dffaadf824c0524397e30
Jamie <134328492+compa-c@users.noreply.github.com>
2023-07-26 12:36:20 +0100

    add the ability to remove a +1

--- webroot/plugins/postplusone/page_plusone.php
+++ webroot/plugins/postplusone/page_plusone.php
@@ -20,8 +20,17 @@ if(!$thread)
 //	die(__("Thread is closed"));
 
 $vote = Fetch(Query("SELECT * FROM {postplusones} WHERE post = {0} AND user = {1}", $pid, $loguserid));
+
+// hope to god I didn't break anything here -- nyaoran_ji
 if($vote)
-	die(__("You already +1'd this"));
+	{
+	Query("UPDATE {posts} SET postplusones = postplusones-1 WHERE id = {0} LIMIT 1", $pid);
+	Query("UPDATE {users} SET postplusones = postplusones-1 WHERE id = {0} LIMIT 1", $post["user"]);
+	Query("UPDATE {users} SET postplusonesgiven = postplusonesgiven-1 WHERE id = {0} LIMIT 1", $loguserid);
+	Query("DELETE FROM {postplusones} WHERE user = {0} AND post = {1}", $loguserid, $pid);
+	recalculateKarma($post["user"]);
+	die(__("+1 removed."));
+}
 
 if($loguser["powerlevel"] < 0)
 	die(__("You are banned and can't give +1's to posts."));
